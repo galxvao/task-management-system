7f1bde405b13e6e74384e2191c1bd9602369e752
DAVI ALEX PAIVA GALVAO <davi_galvao@estudante.sc.senai.br>
2025-03-17 20:15:18 -0300

    creating the entities

---
+++ src/main/java/com/senai/task/models/TaskModel.java
@@ -0,0 +1,36 @@
+package com.senai.task.models;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tarefas")
+public class TaskModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "data")
+    private Date data;
+
+    enum Status {
+        EM_ABERTO, PENDENTE, CONCLUIDO, CANCELADO;
+    }
+
+    @Column(name = "status")
+    private Status status;
+
+
+
+
+
+
+
+}
